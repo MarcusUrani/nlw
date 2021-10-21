1837edcafec1109d5cd35b85ee649726ce5b4ff8
unknown <marcus.urani20@gmail.com>
2021-10-21 11:03:14 -0300

    Adicionando a funcionalidade de mudar os links das redes sociais, por meio do JavaScript

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const social = {
-    github: "MarcusUrani",
+const linksSocialMedia = {
     youtube: "channel/UC436uebW2EWe1LsoNSrAXBw",
-    instagram: "marcusvgu"
+    facebook:"maykbrito",
+    instagram: "marcusvgu",
+    twitter: "maykbrito"
 }
+
+const socialLinks = document.querySelector('.socialLinks');
+
+function changeSocialMediaLinks() {
+    for(let li of socialLinks.children) {
+        const social = li.getAttribute('class');
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    }
+}
+
+changeSocialMediaLinks();
