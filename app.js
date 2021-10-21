b43fccd97f25a7aef1bd7c1b133acf277ec1446e
Marcus Urani <marcus.urani20@gmail.com>
2021-10-21 20:41:56 -0300

    Finalizando o cracha

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const linksSocialMedia = {
     twitter: "maykbrito"
 }
 
-const socialLinks = document.querySelector('.socialLinks');
-
 function changeSocialMediaLinks() {
     for(let li of socialLinks.children) {
         const social = li.getAttribute('class');
@@ -15,3 +13,21 @@ function changeSocialMediaLinks() {
 }
 
 changeSocialMediaLinks();
+
+const github = 'MarcusUrani'
+
+function getUserInfos() {
+    const urlApiGithub = `https://api.github.com/users/${github}`
+    fetch(urlApiGithub)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name;
+        githubUser.textContent = data.login;
+        githubUser.href = data.html_url;
+        githubBio.textContent = data.bio;
+    })
+
+    console.log(urlApiGithub);
+}
+
+getUserInfos()
